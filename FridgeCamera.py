4a092d86133caa866593727a940d26301e65460b
zuluzza <zuluzza@gmail.com>
2020-05-15 14:07:03 +0300

    some fixes

--- FridgeCamera.py
+++ FridgeCamera.py
@@ -1,35 +1,37 @@
-from piCamera import PiCamera
+from picamera import PiCamera
 from time import sleep
 from datetime import datetime
+import os
 
 class FridgeCamera():
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         self._camera = PiCamera();
         self._capturingVideo = False
 
-    def takePicture(filename):
+    def takePicture(self, filename):
         if (self._capturingVideo):
             return
-        self._camera.resolution = (2592, 1944)
-        self._camera.start_preview()
-        # at least 2 secs sleeps is required for camera to sense light levels
-        sleep(2)
-        self._camera.annotate_text = datetime.now
-        self._camera.capture(filename)
-        self._camera.stop_preview()
+        with self._camera as camera:
+            camera.resolution = (2592, 1944)
+            camera.start_preview()
+            # at least 2 secs sleeps is required for camera to sense light levels
+            sleep(2)
+            camera.annotate_text = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            camera.capture(filename)
+            camera.stop_preview()
 
-    def startVideo(filename):
+    def startVideo(self, filename):
         if (self._capturingVideo):
             return
         self._capturingVideo = True
-        self._camera.resolution = (1920, 1080)
-        self._camera.start_preview()
-        self._camera.annotate_text = datetime.now
-        self._camera.startRecording(filename)
+        filename = "/home/pi/smart-fridge/" + filename + ".h264"
+        with self._camera as camera:
+            camera.resolution = (1920, 1080)
+            camera.annotate_text = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            camera.start_recording(filename)
 
-    def endVideo():
+    def endVideo(self):
         if (self._capturingVideo):
             self._capturingVideo = False
             self._camera.stop_recording()
--- PhotoService.py
+++ PhotoService.py
@@ -1,24 +1,28 @@
-import FridgeCamera
+from FridgeCamera import FridgeCamera
 from datetime import datetime
 from time import sleep
+import time
 
-class PhotoService(self):
+class PhotoService:
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        self.stop = False
         self._camera = FridgeCamera()
 
     def run(self):
         while not self.stop:
             #TODO replace with door open detection to start a video
-            self._camera.startVideo("video_{}".format(datetime.now().strftime(strftime("%m/%d/%Y_%H:%M:%S"))))
+            videoName = "video_{}".format(datetime.now().strftime("%m.%d.%Y_%H:%M:%S"))
+            self._camera.startVideo(videoName)
             sleep(10)
-            self.camera.stopVideo()
-
+            self._camera.endVideo()
+            
+            self._camera.takePicture("picture_{}".format(datetime.now().strftime("%m.%d.%Y_%H:%M:%S")))
+        
     def stop(self):
         self.stop = True
-
+        
 #TODO for testing now, to be removed when usage is moved up to a higher lever
 if __name__ == "__main__":
     photoService = PhotoService()
-    photoService.run
+    photoService.run()
     exit(0)
