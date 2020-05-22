55098f38747193233476254bc2cbd5e908d52869
zuluzza <zuluzza@gmail.com>
2020-05-22 14:35:05 +0300

    Add simple video upload to dropbox

--- PhotoService.py
+++ PhotoService.py
@@ -3,10 +3,28 @@ from datetime import datetime
 from time import sleep
 import time
 
+class DropboxCloud:
+    def __init__(self, access_token):
+        self._accessToken = access_token
+
+    def uploadFile(filename):
+        dbx = dropbox.Dropbox(self._accessToken)
+
+        with open(filename, "rb") as file:
+            # this is for uploading < 150MB files. Let's trust it's enough
+            dbx.files_upload(file, "smart_fridge/" + filename)
+
 class PhotoService:
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         self.stop = False
         self._camera = FridgeCamera()
+        self._dropboxCloud = None
+
+    def initDropboxCloud(self):
+        # The file is expected to contain the token only on first line. This whole token handling could be improved
+        with open("dropbox_token.txt", "r") as file:
+            token = file.readline()
+            self._dropboxCloud = DropboxCloud(token.strip())
 
     def run(self):
         while not self.stop:
@@ -15,12 +33,13 @@ class PhotoService:
             self._camera.startVideo(videoName)
             sleep(10)
             self._camera.endVideo()
-            
-            self._camera.takePicture("picture_{}".format(datetime.now().strftime("%m.%d.%Y_%H:%M:%S")))
+            if self._dropboxCloud != None:
+                self._dropboxCloud.uploadFile(filename)
+            #self._camera.takePicture("picture_{}".format(datetime.now().strftime("%m.%d.%Y_%H:%M:%S")))
         
     def stop(self):
         self.stop = True
-        
+
 #TODO for testing now, to be removed when usage is moved up to a higher lever
 if __name__ == "__main__":
     photoService = PhotoService()
